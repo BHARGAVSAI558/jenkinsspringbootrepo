05454459eda31b89e196004d7d043c1754b63a04
BHARGAVSAI558 <bhargavsai954@gmail.com>
2025-08-10 13:48:35 +0530

    updated project

--- src/main/java/com/klef/ClientController.java
+++ src/main/java/com/klef/ClientController.java
@@ -16,4 +16,9 @@ public class ClientController {
 	public String demo() {
 		return "Jenkins Demo";
 	}
+	
+	@GetMapping("/test")
+	public String test() {
+		return "Deploy Spring Boot using Jenkins";
+	}
 }
